389b075e5945ccf04deae90407916fb2b9e14bfd
Noam <NoamStanis@users.noreply.github.com>
2019-11-15 13:31:28 -0500

    Updated Notes

--- Fifteen.py
+++ Fifteen.py
@@ -47,7 +47,10 @@ class Fifteen(QWidget):
         self.r_list = [[], [], [], []]
         self.moves = 0
         self.num_board = []
-        self.solved = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, ' ']]
+        self.solved = [[1, 2, 3, 4],
+                       [5, 6, 7, 8],
+                       [9, 10, 11, 12],
+                       [13, 14, 15, ' ']]
         self.scramble()
 
         self.progress = QProgressBar(self)
